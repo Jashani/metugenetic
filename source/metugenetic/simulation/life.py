d84d7d156cd0ecd8e952245e304f73fd56092eb0
Yasha <yashar@rezilion.com>
2022-11-17 21:08:16 -0500

    Allow a non-torus world in life.py; enforce max iterations

--- source/metugenetic/simulation/life.py
+++ source/metugenetic/simulation/life.py
@@ -1,5 +1,4 @@
-
-MAX_ITERATIONS = 1000
+from metugenetic.config import config
 
 
 class Life:
@@ -12,20 +11,29 @@ class Life:
 
     def simulate_until_repeat(self):
         configurations = [self.cells]
-        for iteration in range(MAX_ITERATIONS):
+        for iteration in range(config.max_iterations):
             self.cycle()
             if self.cells in configurations:
                 return iteration
             configurations.append(self.cells)
+        print(f"WARNING: reached max iterations")
+        return config.max_iterations
 
     def cycle(self):
-        candidates = [(cell + position) % self._grid_size
+        candidates = [self._candidate_position(cell, position)
                       for position in self._neighbour_positions
                       for cell in self.cells]
         self.cells = set(cell for cell in candidates if self._should_birth(candidates, cell))
 
+    def _candidate_position(self, cell, position):
+        if config.is_torus:
+            return (cell + position) % self._grid_size
+        return cell + position
+
     def _should_birth(self, candidates, cell):
-        return 2 - (cell in self.cells) < candidates.count(cell) < 4
+        should_birth = 2 - (cell in self.cells) < candidates.count(cell) < 4
+        within_bounds = 0 <= cell < self._grid_size
+        return within_bounds and should_birth
 
     def print(self):
         for row in range(self._size):
